a8ff05df353b9fce246f14083a483b8b2aa7906a
rajan5787 <rajandp36@gmail.com>
2016-06-17 22:57:28 +0530

    Student activities page simple UI and some code

---
+++ app/src/main/java/com/example/rajan/techsmith/adapter/Select_student_adapter.java
@@ -0,0 +1,7 @@
+package com.example.rajan.techsmith.adapter;
+
+/**
+ * Created by rajan on 16/6/16.
+ */
+public class Select_student_adapter {
+}
---
+++ app/src/main/java/com/example/rajan/techsmith/database/Studentlist.java
@@ -0,0 +1,7 @@
+package com.example.rajan.techsmith.database;
+
+/**
+ * Created by rajan on 16/6/16.
+ */
+public class Studentlist {
+}
